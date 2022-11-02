9ce1a9d26053b7cb97c0581158f9f782ae701f50
= <=>
2022-11-01 21:48:11 -0300

    Clock code load

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Helmet } from 'react-helmet';
+import ClockLogic from './Clock25+5/Clock';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+    
+      <ClockLogic />
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Clock25+5/wason.js
@@ -0,0 +1,7 @@
+
+function wason(n){
+ return n % 60 %60
+} 
+
+
+console.log(wason("1499"))
